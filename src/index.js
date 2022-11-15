ad71c506bb87b3d1ab779dbdd0f9cfb2f88af906
Victor Azevedo <josevcazevedo@gmail.com>
2022-11-15 16:07:57 -0300

    feat: add route POST /sign-up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import Joi from "joi";
+import bcrypt from "bcrypt";
 dotenv.config();
 
 const app = express();
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const registerSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
+  username: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().required(),
   password: Joi.string().required().min(6).max(12),
   repeatPassword: Joi.ref("password"),
@@ -43,6 +44,36 @@ const db = mongoClient.db("myWallet");
 const usersCollection = db.collection("users");
 const movementsCollection = db.collection("movements");
 const sessionsCollection = db.collection("sessions");
+
+app.post("/sign-up", async (req, res) => {
+  const { username, email, password, repeatPassword } = req.body;
+
+  const newUser = { username, email, password, repeatPassword };
+  const { error } = registerSchema.validate(newUser, { abortEarly: false });
+  if (error) {
+    console.log(error.details.map((detail) => detail.message));
+    res.sendStatus(422);
+    return;
+  }
+
+  try {
+    const newUserFind = await usersCollection.findOne({ email: email });
+    if (newUserFind) {
+      return res.sendStatus(409);
+    }
+
+    const passwordHash = bcrypt.hashSync(password, 10);
+    await usersCollection.insertOne({
+      username,
+      email,
+      password: passwordHash,
+    });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+  res.sendStatus(201);
+});
 
 app.listen(process.env.API_PORT, () => {
   console.log(`Server listening on PORT ${process.env.PORT}`);
