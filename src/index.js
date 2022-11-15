62faf9dd33f0d97634554cd73375bcd21960bdcb
Victor Azevedo <josevcazevedo@gmail.com>
2022-11-15 14:31:11 -0300

    feat: add shemas login, register and moviment

--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import Joi from "joi";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const registerSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(20).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required().min(6).max(12),
+  repeatPassword: Joi.ref("password"),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required().min(6).max(12),
+});
+
+const movimentTypes = ["revenue", "expense"];
+const movimentSchema = Joi.object({
+  value: Joi.string().email().required(),
+  description: Joi.string().required().min(4).max(30),
+  type: Joi.alternatives().try(...movimentTypes),
+});
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 mongoClient
