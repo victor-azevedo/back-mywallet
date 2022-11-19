eea32fa82cb0859d2ecda1f97736f62cdbbaced1
Victor Azevedo <josevcazevedo@gmail.com>
2022-11-18 22:00:18 -0300

    refactor: creat transactionsCotrollers

---
+++ src/controllers/transactionsControllers.js
@@ -0,0 +1,79 @@
+import { transactionsCollection, sessionsCollection } from "../database/db.js";
+import { transactionSchema } from "../index.js";
+
+export async function addTransaction(req, res) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+  const { value, description, type, date } = req.body;
+
+  if (!token) return res.sendStatus(401);
+
+  const transaction = {
+    value: Number(value).toFixed(2),
+    description,
+    type,
+    date,
+  };
+  const { error } = transactionSchema.validate(transaction, {
+    abortEarly: false,
+  });
+  if (error) {
+    console.log(error.details.map((detail) => detail.message));
+    res.sendStatus(422);
+    return;
+  }
+
+  try {
+    const session = await sessionsCollection.findOne({ token });
+    if (!session) {
+      return res.sendStatus(401);
+    }
+
+    await transactionsCollection.insertOne({
+      userId: session.userId,
+      ...transaction,
+    });
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function getTransactions(req, res) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) return res.sendStatus(401);
+
+  try {
+    const session = await sessionsCollection.findOne({ token });
+    if (!session) {
+      return res.sendStatus(401);
+    }
+
+    const transactionsUser = await transactionsCollection
+      .find({ userId: session.userId })
+      .toArray();
+
+    const values = transactionsUser.map((transaction) => {
+      if (transaction.type === "incoming") {
+        return Number(transaction.value);
+      } else if (transaction.type === "outgoing") {
+        return Number(-transaction.value);
+      }
+    });
+
+    const balance = values.reduce((acc, value) => acc + value, 0).toFixed(2);
+    transactionsUser.forEach((transaction) => delete transaction.userId);
+
+    res.status(200).send({
+      userId: session.userId,
+      balance,
+      transactions: transactionsUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import Joi from "joi";
-import {
-  usersCollection,
-  transactionsCollection,
-  sessionsCollection,
-} from "./database/db.js";
 import { registerUser, login, logout } from "./controllers/usersController.js";
+import {
+  addTransaction,
+  getTransactions,
+} from "./controllers/transactionsControllers.js";
 dotenv.config();
 
 const app = express();
@@ -40,82 +39,9 @@ app.post("/sign-in", login);
 
 app.delete("/logout", logout);
 
-app.post("/transactions", async (req, res) => {
-  const { authorization } = req.headers;
-  const token = authorization?.replace("Bearer ", "");
-  const { value, description, type, date } = req.body;
+app.post("/transactions", addTransaction);
 
-  if (!token) return res.sendStatus(401);
-
-  const transaction = {
-    value: Number(value).toFixed(2),
-    description,
-    type,
-    date,
-  };
-  const { error } = transactionSchema.validate(transaction, {
-    abortEarly: false,
-  });
-  if (error) {
-    console.log(error.details.map((detail) => detail.message));
-    res.sendStatus(422);
-    return;
-  }
-
-  try {
-    const session = await sessionsCollection.findOne({ token });
-    if (!session) {
-      return res.sendStatus(401);
-    }
-
-    await transactionsCollection.insertOne({
-      userId: session.userId,
-      ...transaction,
-    });
-    res.sendStatus(201);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-});
-
-app.get("/transactions", async (req, res) => {
-  const { authorization } = req.headers;
-  const token = authorization?.replace("Bearer ", "");
-
-  if (!token) return res.sendStatus(401);
-
-  try {
-    const session = await sessionsCollection.findOne({ token });
-    if (!session) {
-      return res.sendStatus(401);
-    }
-
-    const transactionsUser = await transactionsCollection
-      .find({ userId: session.userId })
-      .toArray();
-
-    const values = transactionsUser.map((transaction) => {
-      if (transaction.type === "incoming") {
-        return Number(transaction.value);
-      } else if (transaction.type === "outgoing") {
-        return Number(-transaction.value);
-      }
-    });
-
-    const balance = values.reduce((acc, value) => acc + value, 0).toFixed(2);
-    transactionsUser.forEach((transaction) => delete transaction.userId);
-
-    res.status(200).send({
-      userId: session.userId,
-      balance,
-      transactions: transactionsUser,
-    });
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-});
+app.get("/transactions", getTransactions);
 
 app.listen(process.env.API_PORT, () => {
   console.log(`Server listening on PORT ${process.env.PORT}`);
