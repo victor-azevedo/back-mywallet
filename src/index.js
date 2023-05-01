052ae65313d6bae6f202c900bd0ed57100a3805f
Victor Azevedo <josevcazevedo@gmail.com>
2023-05-01 16:23:40 -0300

    feat: set default PORT

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import usersRoutes from "./routes/usersRoutes.js";
+import express from "express";
+
 import transactionsRoutes from "./routes/transactionsRoutes.js";
+import usersRoutes from "./routes/usersRoutes.js";
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +13,8 @@ app.use(express.json());
 app.use(usersRoutes);
 app.use(transactionsRoutes);
 
-app.listen(process.env.API_PORT, () => {
-  console.log(`Server listening on PORT ${process.env.API_PORT}`);
+const PORT = process.env.API_PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
 });
