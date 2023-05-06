cadc035c44b287f282ccc5b55dcccebe01d5595b
Victor Azevedo <josevcazevedo@gmail.com>
2023-05-06 14:12:46 -0300

    feat: update docker compose

--- src/database/db.js
+++ src/database/db.js
@@ -3,17 +3,20 @@ import { MongoClient } from "mongodb";
 
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const mongoClient = new MongoClient(
+  `${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`
+);
 mongoClient
   .connect()
   .then(() => {
     console.log("Connected successfully to data server");
   })
-  .catch(() => {
+  .catch((err) => {
     console.error("ERROR: Not connected to data server");
+    console.log(err);
   });
 
-const db = mongoClient.db("myWallet");
+const db = mongoClient.db(process.env.MONGO_DATABASE);
 export const usersCollection = db.collection("users");
 export const transactionsCollection = db.collection("transactions");
 export const sessionsCollection = db.collection("sessions");
